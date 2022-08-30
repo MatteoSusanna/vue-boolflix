3e1cbaad0d2dc05f47d59d6415cbffa2c8e00831
Matteo Susanna <matteo2784@gmail.com>
2022-08-30 14:34:26 +0200

    Chiamata axios

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,11 @@
 <template>
-  <header></header>
+  <header>
+    <h1>BOOLFLIX</h1>
+    <div>
+        <input type="text">
+        <button>Cerca</button>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,6 +14,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../styles/general.scss';
+
+    header{
+        height: 100px;
+        background-color: $header_color;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 50px;
+        h1{
+            color: red;
+            font-weight: 500;
+        }
+    }
 
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,13 +1,36 @@
 <template>
-  <main></main>
+  <main>
+
+  </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    nome: 'MyMain'
+    nome: 'MyMain',
+    data(){
+        return{
+            getFilm: [],
+        }
+    },
+    created(){
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=976d1d1629fea964df856f00c768c591&query=2021&language=it-IT')
+        .then(film =>{
+            this.getFilm = film
+        })
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../styles/general.scss';
+
+    main{
+        height: calc(100vh - 100px);
+        background-color: $main_color;
+        overflow: hidden;
+        overflow-y: scroll;
+    }
 
 </style>
