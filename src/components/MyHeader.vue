f8684e1ce38b6343d66377e0f00b88dcabf8b110
Matteo Susanna <matteo2784@gmail.com>
2022-08-30 14:22:13 +0200

    Inizio esercizio

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+    nome: 'MyHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+    nome: 'MyMain'
+}
+</script>
+
+<style>
+
+</style>
