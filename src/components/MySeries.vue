7d547d96623e34698e081887ba230d28444815ee
Matteo Susanna <matteo2784@gmail.com>
2022-09-01 11:10:46 +0200

    refactoring stelline

--- src/components/MySeries.vue
+++ src/components/MySeries.vue
@@ -37,8 +37,9 @@
 
                         <div class="punteggio t-center">
                             <i v-for="n in 5" :key="n"
-                            class="fa-star" 
-                            :class="(n>calcolaVoto(serie.vote_average))?'fa-regular':'fa-solid'"></i>
+                                class="fa-star" 
+                                :class="(n>calcolaVoto(serie.vote_average))?'fa-regular':'fa-solid'">
+                            </i>
                         </div>
 
                         <h3 class="text_center">Trama:</h3>
